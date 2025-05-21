9b1952c1d0952f87c80d51d279dd62b8709b9ff4
OrcDev <urosmiric88@gmail.com>
2025-05-21 21:31:43 +0200

    feat: add new ModeSwitcher component to replace ModeToggle in header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,7 +1,7 @@
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { ModeToggle } from "./ui/mode-toggle";
 import { ThemeSelector } from "./theme-selector";
+import { ModeSwitcher } from "./mode-switcher";
 
 export function SiteHeader() {
   return (
@@ -15,7 +15,7 @@ export function SiteHeader() {
         <h1 className="text-base font-medium">Documents</h1>
         <div className="ml-auto flex items-center gap-2">
           <ThemeSelector />
-          <ModeToggle />
+          <ModeSwitcher />
         </div>
       </div>
     </header>
